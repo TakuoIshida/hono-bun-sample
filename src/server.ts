b2586a55187c28e447ad58382914650785100c30
TakuoIshida <archersparadox.100@gmail.com>
2025-07-15 11:29:17 +0900

    package.jsonにクライアントスクリプトを追加し、サーバーにユーザー管理APIを実装しました。新しい`src/client.ts`ファイルを作成し、`src/server.ts`にテストデータとAPIエンドポイントを追加しました。

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono() // これの下に記述
 
-app.get('/', (c) => {
+// 適当なテストデータ追加
+const users = [
+  {id: 1, name: 'tarou', age: 15},
+  {id: 2, name: 'hanako', age: 20},
+]
+
+// API追加
+app.post('/api/users', async (c) => {
+  const user = await c.req.json()
+
+  users.push({id: users.length + 1, ...user})
+  return c.json(user)
+}).get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+
+export type AppType = typeof app
 
 export default app
